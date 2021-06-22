f2e40a6d303be1f161059141ddbb36ec7828a3b3
justin <>
2021-06-22 19:34:02 +0800

    feat: add format in script

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogger } from 'redux-logger';
 import Header from 'Components/Header';
+import rootSaga from './sagas';
 import './App.scss';
+
+const logger = createLogger();
 
 interface IProps {
   name: string;
   age: number;
 }
 
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(() => {
+  return {};
+}, composeWithDevTools(applyMiddleware(logger, sagaMiddleware)));
+sagaMiddleware.run(rootSaga);
+
 function App(props: IProps) {
   const { name, age } = props;
   return (
-    <div className='app'>
-      <Header />
-      <span>{`Hello! I'm ${name}, ${age} yearssss old.`}</span>
-    </div>
+    <Provider store={store}>
+      <div className='app'>
+        <Header />
+        <span>{`Hello! I'm ${name}, ${age} yearssss old.`}</span>
+      </div>
+    </Provider>
   );
 }
 
