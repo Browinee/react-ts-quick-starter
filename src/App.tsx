df2630e8c167153a8e27afec30aa9627417cc630
justin.lin <justin.lin@yxunistar.com>
2021-06-23 12:29:07 +0800

    feat: add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import { applyMiddleware, createStore } from 'redux';
@@ -25,11 +26,27 @@ function App(props: IProps) {
   const { name, age } = props;
   return (
     <Provider store={store}>
-      <div className='app'>
-        <Header />
-        <span>{`Hello! I'm ${name}, ${age} yearssss old.`}</span>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={() => {
+            return (
+              <div className='app'>
+                <Header />
+                <span>{`Hello! I'm ${name}, ${age} yearssss old.`}</span>
+              </div>
+            )
+          }} />
+          {/*<ModuleRoute path={`${config.basePath}/${ModuleNames.account}`} component={AccountModuleRootComponent} />*/}
+          {/*<ModuleRoute path={`${config.basePath}/${ModuleNames.withdraw}`} component={WithdrawModuleRootComponent} />*/}
+          {/*/!*NOTE: 預設轉導到提現的提現子頁面*!/*/}
+          {/*<ModuleRedirect to={`${config.basePath}/${ModuleNames.withdraw}`} />*/}
+          {/*NOTE: 預設轉導到找不到子頁面*/}
+          {/*<ModuleRoute component={NotFoundPage} />*/}
+          <Redirect to="/login"/>
+        </Switch>
+      </BrowserRouter>
     </Provider>
+
   );
 }
 
