708734531ebc002c0c837829a891e9f940e58798
justin <>
2021-07-08 18:28:23 +0800

    feat: add errorboundary

---
+++ src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import FallbackPage from 'Components/FallbackPage';
+
+class ErrorBoundary extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    // 錯誤記錄到一個錯誤回報系統服務
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <FallbackPage />;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
