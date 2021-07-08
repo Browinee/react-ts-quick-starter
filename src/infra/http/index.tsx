fcfc17b1b5fc7251abc7f3b080e07dad2d0e667d
justin <>
2021-07-08 18:23:18 +0800

    feat: add global error in axios

--- src/infra/http/index.tsx
+++ src/infra/http/index.tsx
@@ -8,9 +8,17 @@ service.interceptors.request.use((config) => {
   return config;
 });
 
-service.interceptors.response.use((response) => {
-  const { data } = response;
-  return data;
-});
+service.interceptors.response.use(
+  (response) => {
+    const { data } = response;
+    return data;
+  },
+  (err) => {
+    if (err.response.status === 403) {
+      // 跳轉到登入頁
+    }
+    return Promise.reject(err);
+  },
+);
 
 export default service;
