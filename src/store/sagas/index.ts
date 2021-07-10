d64575ccd4da1c4f3ea58a2bfb37892e7f7e4444
justin <>
2021-07-10 12:19:34 +0800

    chore: typo

--- src/store/sagas/index.ts
+++ src/store/sagas/index.ts
@@ -1,6 +1,5 @@
 import { all } from 'redux-saga/effects';
-import { watchLogin } from '../../module/auth/store/sagas/login';
-
+import {watchLogin} from "../../module/auth/store/sagas/login";
 export default function* rootSaga() {
   yield all([watchLogin()]);
 }
